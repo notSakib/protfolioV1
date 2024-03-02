962ddddec5f4d7f44253b506a5c47aaf24fb213c
Md Saban <md.saban@atlan.com>
2024-03-02 12:36:16 +0530

    feat: add sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,16 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import expressiveCode from "astro-expressive-code";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mdsaban.com',
   markdown: {
     // Can be 'shiki' (default), 'prism' or false to disable highlighting
     // syntaxHighlight: 'prism'
   },
-  integrations: [
-    tailwind({
-      // applyBaseStyles: false
-    }), 
-    expressiveCode(),
-    mdx(), 
-  ]
+  integrations: [tailwind({
+    // applyBaseStyles: false
+  }), expressiveCode(), mdx(), sitemap()]
 });
