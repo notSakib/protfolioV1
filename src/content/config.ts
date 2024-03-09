ffaee21c097b319cb94d3a4c60b0f346b8e0c555
Md Saban <md.saban@atlan.com>
2024-03-09 13:27:26 +0530

    design and seo related changes

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,19 +2,20 @@
 import { z, defineCollection } from "astro:content";
 // Define a `type` and `schema` for each collection
 const postsCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-      title: z.string(),
-      pubDate: z.date(),
-      description: z.string(),
-      author: z.string(),
-      image: z.object({
-        url: z.string(),
-        alt: z.string()
-      }),
-      tags: z.array(z.string()),
-      noindex: z.boolean(),
-    })
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    seoTitle: z.string().optional(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+    noindex: z.boolean().optional(),
+  }),
 });
 // Export a single `collections` object to register your collection(s)
 export const collections = {
